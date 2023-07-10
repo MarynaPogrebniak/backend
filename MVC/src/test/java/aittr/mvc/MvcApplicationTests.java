fbf26c1da2fb8b59e58af5eb7e149de0822fbb5a
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-07-10 11:00:05 +0200

    create MVC project

---
+++ MVC/src/test/java/aittr/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package aittr.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
