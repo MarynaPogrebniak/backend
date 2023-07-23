5d30475fc7982ec4aeeea8a951586b620630e75e
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-07-23 14:32:40 +0200

    update

---
+++ REST API/REST API/src/main/java/de/ait/timepad/dto/NewUserDto.java
@@ -0,0 +1,9 @@
+package de.ait.timepad.dto;
+
+import lombok.Data;
+
+@Data
+public class NewUserDto {
+    private String email;
+    private String password;
+}
---
+++ REST API/REST API/src/main/java/de/ait/timepad/models/Event.java
@@ -0,0 +1,28 @@
+package de.ait.timepad.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Event {
+
+    public enum State {
+
+        CREATED,
+        IN_PROGRESS,
+        COMPLETED,
+        DELETED
+    }
+
+    private Long id;
+    private String name;
+    private String location;
+    private Integer price;
+
+    private State state;
+}
