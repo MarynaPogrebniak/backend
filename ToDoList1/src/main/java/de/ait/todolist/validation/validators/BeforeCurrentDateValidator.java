7c1879c7e430cda08db1721a29b77776049de7fd
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-08-08 14:20:30 +0200

    update

--- ToDoList1/src/main/java/de/ait/todolist/validation/validators/BeforeCurrentDateValidator.java
+++ ToDoList1/src/main/java/de/ait/todolist/validation/validators/BeforeCurrentDateValidator.java
@@ -6,13 +6,23 @@ import jakarta.validation.ConstraintValidatorContext;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class BeforeCurrentDateValidator implements ConstraintValidator<BeforeCurrentDate, String> {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     @Override
     public boolean isValid(String checkDate, ConstraintValidatorContext constraintValidatorContext) {
+        if (checkDate == null || checkDate.trim().isEmpty()) {
+            return true;
+        }
 
-        LocalDate checkDateLD = LocalDate.parse(checkDate, formatter);
+        LocalDate checkDateLD;
+        try {
+            checkDateLD = LocalDate.parse(checkDate, formatter);
+        } catch (RuntimeException e) {
+            return false;
+        }
+
         LocalDate currentDate = LocalDate.now();
 
         return checkDateLD.isEqual(currentDate) || checkDateLD.isAfter(currentDate);
