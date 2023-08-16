3f3b76fe17bdb4c8c8dac020653eec7c3d3920a9
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-08-16 14:33:02 +0200

    add EducationCenter project

---
+++ EducationCenter/src/main/java/de/ait/ec/models/Lesson.java
@@ -0,0 +1,36 @@
+package de.ait.ec.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    private LocalTime startTime;
+
+    private LocalTime finishTime;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+}
